b99674d20805a440efb33d7a89bdc0ba2383d82f
Robert Schütz <nix@dotlambda.de>
2025-12-01 22:54:40 -0800

    python3Packages.pyflick: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13285,8 +13285,6 @@ self: super: with self; {
 
   pyflic = callPackage ../development/python-modules/pyflic { };
 
-  pyflick = callPackage ../development/python-modules/pyflick { };
-
   pyflipper = callPackage ../development/python-modules/pyflipper { };
 
   pyfluidsynth = callPackage ../development/python-modules/pyfluidsynth { };
